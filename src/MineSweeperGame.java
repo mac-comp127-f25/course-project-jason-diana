af029be24a700a3327fb5123fdbee4b542beac5b
DIANA <yhu1@macalester.edu>
2025-11-24 10:13:51 -0600

    drawing the board

--- src/MineSweeperGame.java
+++ src/MineSweeperGame.java
@@ -3,10 +3,9 @@ import java.awt.Color;
 
 
 public class MineSweeperGame {
-    private static final int CANVAS_WIDTH = 700;
-    private static final int CANVAS_HEIGHT = 800;
-    private static final int CELL_SIZE = 30;
-    private static final int SPACING = 5;
+    private static final int CANVAS_WIDTH = 500;
+    private static final int CANVAS_HEIGHT = 500;
+
 
     private CanvasWindow canvas;
     private Board board = new Board(10,10, 15);
@@ -32,58 +31,61 @@ public class MineSweeperGame {
     }
 
     private void drawBoard() {
+        double ox = 100;
+        double oy = 100;
+        Image imgii = new Image("0.png");
         for (int i = 0; i < board.getRow(); i++) {
             for (int j = 0; j < board.getCol(); j++) {
-                double x = SPACING + i * CELL_SIZE;
-                double y = SPACING + j * CELL_SIZE;
+                double x = ox+ i*imgii.getHeight();
+                double y = oy + j*imgii.getWidth();
 
                 Cell cell = board.getCell(i, j);
 
                 if (cell.isRevealed) {
                     if (!cell.isMine) {
                         if (cell.getMineNum() == 0) {
-                        Image img = new Image(x, y, "res/0.png");
+                        Image img = new Image(x, y, "0.png");
                         canvas.add(img);
                         }
                         if (cell.getMineNum() == 1) {
-                            Image img = new Image(x, y, "res/1.png");
+                            Image img = new Image(x, y, "1.png");
                             canvas.add(img);
                         }
                         if (cell.getMineNum() == 2) {
-                            Image img = new Image(x, y, "res/2.png");
+                            Image img = new Image(x, y, "2.png");
                             canvas.add(img);
                         }
                         if (cell.getMineNum() == 3) {
-                            Image img = new Image(x, y, "res/3.png");
+                            Image img = new Image(x, y, "3.png");
                             canvas.add(img);
                         }
                         if (cell.getMineNum() == 4) {
-                            Image img = new Image(x, y, "res/4.png");
+                            Image img = new Image(x, y, "4.png");
                             canvas.add(img);
                         }
                         if (cell.getMineNum() == 5) {
-                            Image img = new Image(x, y, "res/5.png");
+                            Image img = new Image(x, y, "5.png");
                             canvas.add(img);
                         }
                         if (cell.getMineNum() == 6) {
-                            Image img = new Image(x, y, "res/6.png");
+                            Image img = new Image(x, y, "6.png");
                             canvas.add(img);
                         }
                         if (cell.getMineNum() == 7) {
-                            Image img = new Image(x, y, "res/7.png");
+                            Image img = new Image(x, y, "7.png");
                             canvas.add(img);
                         }
                         if (cell.getMineNum() == 8) {
-                            Image img = new Image(x, y, "res/8.png");
+                            Image img = new Image(x, y, "8.png");
                             canvas.add(img);
                         }
                     }
                     else {
-                        Image img = new Image(x, y, "res/mine.png");
+                        Image img = new Image(x, y, "mine.png");
                         canvas.add(img); 
                     }
                 } else {
-                    Image img = new Image(x, y, "res/10.png");
+                    Image img = new Image(x, y, "10.png");
                     canvas.add(img); 
                 }
             }
